034099adffd764837d981b53f8fd6b3c6143389a
hitesh <hitesh22rana@gmail.com>
2023-06-10 00:01:39 +0530

    refactor: files cleaned

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'OptSys',
+  title: 'OptySys',
   description: 'Opportunity management system',
 }
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
    <div>
-      <h1>OptSys</h1>
+      <h1>OptySys</h1>
    </div>
   )
 }
