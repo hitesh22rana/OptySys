3fef0cc7dce9efa30465a39f4140c68a6ea51493
hitesh <hitesh22rana@gmail.com>
2023-06-10 17:13:44 +0530

    chore: added password hashing functions

---
+++ backend/app/utils/hashing.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+
+class Hasher:
+    @classmethod
+    def __init__(cls):
+        cls.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    @classmethod
+    def verify_password(cls, plain_password, hashed_password):
+        return cls.pwd_context.verify(plain_password, hashed_password)
+
+    @classmethod
+    def get_password_hash(cls, password):
+        return cls.pwd_context.hash(password)
