4a1a32265793574292b0f620330184e7467b832a
sagar karotia <sagarkarotia@gmail.com>
2023-07-20 15:41:01 +0530

    created signUp form

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import FormWrapper from "@/components/auth/FormWrapper";
+
+import { toast } from "react-toastify";
+
+import { SignupFormData } from "@/types/common";
+import { login } from "@/http";
+
 export default function Home() {
+  const [formData, setFormData] = useState<SignupFormData>({
+    email: "",
+    password: "",
+    verify_password:""
+  });
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // checks for formdata
+    if (formData.email === "" || formData.password === "" || formData.verify_password==="") {
+      toast.error("Please fill in all fields");
+      return;
+    }
+    if(formData.password!==formData.verify_password){
+      toast.error("Please fill correct password");
+      return;
+    }
+    try {
+      const { data } = await login(formData);
+      console.log(data);
+    } catch (err) {
+      toast.error("Invalid credentials");
+    }
+  }
+
   return (
-    <div>
-      <h2>Signup</h2>
-    </div>
+    <FormWrapper
+      title="Signup"
+      subtitle="signup to create your account"
+      onSubmit={onSubmit}
+    >
+      <div className="flex flex-col gap-4 w-full">
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          className="outline-none border-2 p-2 rounded"
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          className="outline-none border-2 p-2 rounded"
+          minLength={3}
+          onChange={onChange}
+        />
+        <input
+          name="verify_password"
+          type="password"
+          placeholder="verify password"
+          className="outline-none border-2 p-2 rounded"
+          minLength={3}
+          onChange={onChange}
+        />
+      </div>
+    </FormWrapper>
   );
 }
--- frontend/types/common.ts
+++ frontend/types/common.ts
@@ -4,7 +4,11 @@ export interface FormData {
   email: string;
   password: string;
 }
-
+export interface SignupFormData {
+  email: string;
+  password: string;
+  verify_password:string
+}
 export interface WrapperProps {
   children: string | JSX.Element | Array<JSX.Element> | React.ReactNode;
 }
