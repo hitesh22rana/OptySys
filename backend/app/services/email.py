981ab39255b7a260258730271175df0411a640c2
hitesh <hitesh22rana@gmail.com>
2023-07-22 20:29:23 +0530

    fix: email service timeout issue

--- backend/app/services/email.py
+++ backend/app/services/email.py
@@ -2,6 +2,7 @@
 # Path: backend/app/services/email.py
 
 import smtplib
+import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -20,7 +21,8 @@ class EmailService:
     otp_template = "otp.html"
     cover_letter_template = "cover_letter.html"
     server = None
-    max_attempts = 3
+    last_activity_time = None
+    timeout = 300  # 5 minutes
 
     @classmethod
     def __init__(cls):
@@ -29,20 +31,37 @@ class EmailService:
     @classmethod
     def _login(cls):
         try:
-            if not cls.server or cls.max_attempts > 0:
+            if not cls.server:
                 cls.server = smtplib.SMTP(cls.smtp_server, cls.smtp_port)
                 cls.server.starttls()
                 cls.server.login(cls.sender, cls.password)
-                cls.max_attempts = 3
 
         except smtplib.SMTPException as e:
-            cls.max_attempts -= 1
-            if cls.max_attempts > 0:
-                cls._login()
             logger.error(f"SMTP Error: {e}")
 
         except Exception as e:
             logger.error(f"Error: {e}")
+
+    @classmethod
+    def _check_connection(cls):
+        if cls.last_activity_time is None:
+            cls._reconnect()
+            cls.last_activity_time = time.time()
+
+        current_time = time.time()
+        elapsed_time = current_time - cls.last_activity_time
+
+        if elapsed_time >= cls.timeout:
+            cls._reconnect()
+        else:
+            cls.last_activity_time = current_time
+
+    @classmethod
+    def _reconnect(cls):
+        cls.server.quit()
+        cls.server = None
+        cls.last_activity_time = None
+        cls._login()
 
     @classmethod
     def send_otp(cls, recipient, subject, otp):
@@ -60,6 +79,8 @@ class EmailService:
 
     @classmethod
     def send_email_to_user(cls, recipient, subject, message):
+        cls._check_connection()
+
         try:
             msg = MIMEMultipart("alternative")
             msg["Subject"] = subject
@@ -72,6 +93,8 @@ class EmailService:
             msg.attach(html_content)
 
             cls.server.sendmail(cls.sender, recipient, msg.as_string())
+
+            cls.last_activity_time = time.time()
 
         except smtplib.SMTPException as e:
             logger.error(f"SMTP Error: {e}")
