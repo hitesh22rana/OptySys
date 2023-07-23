e69783f3091c3ba06dd6b9e88b64a4c4ceb69b34
hitesh <hitesh22rana@gmail.com>
2023-07-23 22:55:53 +0530

    fix: large screens width fix

--- frontend/components/dashboard/Sidebar.tsx
+++ frontend/components/dashboard/Sidebar.tsx
@@ -5,8 +5,8 @@ export default function Sidebar() {
 
   return (
     <aside
-      className={`flex flex-col 3xl:w-80 transition-width ease-in-out duration-300 ${
-        isSidebarOpen ? "w-52" : "w-24"
+      className={`flex flex-col transition-width ease-in-out duration-300 ${
+        isSidebarOpen ? "3xl:w-80 w-52" : "3xl:w-40 w-24"
       } h-screen fixed left-0 top-0 bottom-0 p-5 shadow-gray-200 shadow`}
     >
       <div></div>
