60abdb578efc3b6a760e2be1c4ed20e1526f3bdc
hitesh <hitesh22rana@gmail.com>
2023-08-10 00:08:24 +0530

    fix: modal and dialog on open background scroll disabled

--- frontend/src/components/common/AlertDialog.tsx
+++ frontend/src/components/common/AlertDialog.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+
 import { AlertDialogProps } from "@/src/types/common";
 
 export default function AlertDialog({ isOpen, children }: AlertDialogProps) {
+  useEffect(() => {
+    // Add or remove the "overflow: hidden" style to the body based on the dialog's open state
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Clean up the effect
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
     <div
       className={`fixed w-screen h-screen flex items-center justify-center z-[9999] p-3 backdrop-blur inset-0 ${
--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+
 import { ModalProps } from "@/src/types/common";
 
 export default function Modal({ isOpen, children }: ModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
     <div
       className={`fixed w-screen h-screen flex items-center justify-center z-[9999] p-3 backdrop-blur inset-0 ${
