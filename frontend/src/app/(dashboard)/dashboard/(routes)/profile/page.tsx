ea2f36ae9c74009be2ba532950f6a11e178cd6e1
hitesh <hitesh22rana@gmail.com>
2023-08-27 14:54:22 +0530

    feat: added skills section

--- frontend/src/app/(dashboard)/dashboard/(routes)/profile/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/(routes)/profile/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { BiUser } from "react-icons/bi";
+import Select, { MultiValue } from "react-select";
 
 import SectionWrapper from "@/src/components/dashboard/SectionWrapper";
 
 import { useUserStore } from "@/src/stores";
 
+import { IOption } from "@/src/types/common";
+import { defaultSkills, getSkillsList } from "@/src/lib/helpers";
+
 export default function ProfilePage() {
   const { user } = useUserStore();
 
   const [summary, setSummary] = useState<string>(user?.summary);
+
+  const [skills, setSkills] = useState<Array<string>>(user?.skills);
+  const [allSkills, setAllSkills] = useState<Array<IOption>>([]);
+
+  useEffect(() => {
+    async function getSkillsData() {
+      const data = await getSkillsList();
+      setAllSkills(data);
+    }
+
+    getSkillsData();
+  }, []);
 
   function onSummaryChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     const value: string = e.target.value;
@@ -21,6 +37,12 @@ export default function ProfilePage() {
     // const errorMessage = getAboutStepErrors(value, skills);
 
     // setError(errorMessage);
+  }
+
+  function onSkillsChange(option: MultiValue<IOption>) {
+    const currentSkills: Array<string> = option.map((opt) => opt.value);
+
+    setSkills(currentSkills);
   }
 
   return (
@@ -56,6 +78,33 @@ export default function ProfilePage() {
             placeholder="e.g. Experienced technical specialist and IT professional, proficient in systems administration, with a background in programming."
             className="h-auto max-h-32 min-h-[8rem] w-full rounded border-[1px] p-2 text-sm text-gray-500 outline-none placeholder:text-sm placeholder:font-light focus:outline-none focus:ring-1 focus:ring-gray-400"
             onChange={onSummaryChange}
+          />
+        </div>
+
+        <div className="flex h-full w-full flex-col gap-[5px]">
+          <span className="text-xl font-semibold text-gray-700 md:text-3xl">
+            Add skills
+          </span>
+          <Select
+            options={allSkills}
+            isMulti={true}
+            isSearchable={true}
+            onChange={onSkillsChange}
+            backspaceRemovesValue={true}
+            blurInputOnSelect={true}
+            defaultValue={defaultSkills(user?.skills)}
+            styles={{
+              menuList: (provided) => ({
+                ...provided,
+                height: "100%",
+                maxHeight: "170px",
+              }),
+              control: (provided) => ({
+                ...provided,
+                outline: "1px solid rgb(107, 114, 128)",
+                border: "none",
+              }),
+            }}
           />
         </div>
       </section>
